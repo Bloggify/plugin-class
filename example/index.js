0fd409d9dda077b2ee487df18a6951cf07217f40
Ionică Bizău <bizauionica@gmail.com>
2017-06-05 16:44:28 +0300

    :arrow_up: 1.0.1 :tada:

--- example/index.js
+++ example/index.js
@@ -1,17 +1,18 @@
 "use strict";
 
-const BloggifyPluginClass = require("../lib");
+const BloggifyPluginClass = require("../lib")
+    , Bloggify = require("bloggify")
+    ;
 
 // Create a new plugin
 let myPlugin = return new BloggifyPlugin(
     "foo"
   , "path/to/foo"
-    // This should be the Bloggify instance
-  , bloggify
+  , Bloggify
 );
 
 // And initialize it
 myPlugin.init((err, data) => {
     // Do something after initialization
-    ...
+    // ...
 });
